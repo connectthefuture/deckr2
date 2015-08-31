ccef4a826677253d7c97f19e9fde9f2ffdedd78c
alexthebake <abake@uchicago.edu>
2015-08-31 17:00:43 -0500

    Create Game model.

--- clients/webserver/deckr/webclient/models.py
+++ clients/webserver/deckr/webclient/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Game(models.Model):
+    game_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    variant = models.CharField(max_length=50)
+    max_players = models.IntegerField()
+    created_by = models.ForeignKey(User)
+    is_game_over = models.BooleanField(default=False)
