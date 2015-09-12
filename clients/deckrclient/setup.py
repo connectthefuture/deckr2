b2f6a1751a54174a7b7114750aae5fcb04daabb1
Tristan Rasmussen <tristanisthebest@gmail.com>
2015-09-12 18:37:04 -0500

    Added the start of a better client.

---
+++ clients/deckrclient/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='deckrclient',
+      version='0.1',
+      description='A simple deckr client',
+      author='Tristan Rasmussen',
+      license='MIT',
+      packages=['deckrclient'],
+      zip_safe=False)
