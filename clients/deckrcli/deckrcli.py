3f5a683ebafa483b911b52def8240389140129ac
Tristan Rasmussen <tristanisthebest@gmail.com>
2015-09-13 23:04:19 -0500

    Minor fix to deckrcli.

--- clients/deckrcli/deckrcli.py
+++ clients/deckrcli/deckrcli.py
@@ -23,20 +23,20 @@ def main():
     Run main functionality.
     """
 
-    client = deckrclient.client.DeckrClient(ip='127.0.0.1', port=8080,
-                                            sync=False, callback=printer_callback)
+    client = deckrclient.client.DeckrClient(ip='127.0.0.1',
+                                            port=8080,
+                                            sync=False,
+                                            callback=printer_callback)
     client.initialize()
     # Here is the poor man's REPL
     while True:
         try:
             print(input(">>> "))
-        except EOFError:
+        except (EOFError, KeyboardInterrupt): # These are actual exit conditions.
             break
-        except SyntaxError:
-            continue
-        except KeyboardInterrupt:
-            break
-        except: # Keep going with other errors.
+        except SyntaxError: # For some reason a blank line causes a SyntaxError, just pass
+            pass
+        except: # Other exceptions should be logged and then we should continue the REPL
             traceback.print_exc()
     client.shutdown()
 
