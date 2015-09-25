c2e1642ed0bcec292cad279532c7ca9590e4358b
alexthebake <abake@uchicago.edu>
2015-09-25 00:41:04 -0500

    Temporarily fix deck builder (will eventually be replaced)

--- clients/webserver/deckr/webclient/deck_builder.py
+++ clients/webserver/deckr/webclient/deck_builder.py
@@ -2,5 +2,5 @@ def build_deck(deck_text):
     deck = []
     for line in deck_text.split('\n'):
         card = line.split(' ')
-        deck += [str(card[1]) for _ in range(int(card[0]))]
+        deck += [str(' '.join(card[1:])) for _ in range(int(card[0]))]
     return deck
